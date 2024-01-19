a25a346f18363c76d82383cde8d501e8cfc75bf1
Naveenn789 <naveengalla77@gmail.com>
2024-01-19 16:31:25 +0530

    Pizza

---
+++ UnitTestProject/Testing.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ClassLibrary;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class Testing
+    {
+        Pizza p = new Pizza();        
+        [TestMethod]
+        public void Test1()
+        {
+            //Assign or Arrange
+            int pid = 101;
+            string ptype = "Chicken Pizza";
+
+            //Act
+            string acname = p.display(101);
+
+            //Assert
+
+            Assert.AreEqual(ptype, acname);
+        }
+        [TestMethod]
+        public void Test2()
+        {
+
+            //Assign or Arrange
+            int pid = 102;
+            string ptype = "Margherita";
+
+            //Act
+            string acname = p.display(102);
+
+            //Assert
+
+            Assert.AreEqual(ptype, acname);
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            //Assign or Arrange
+            int pid = 102;
+            string ptype = "Margherita";
+
+            //Act
+            string acname = p.display(102);
+
+            //Assert
+
+            Assert.AreEqual(ptype, acname);
+        }
+    }
+}
